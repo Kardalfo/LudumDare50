80a00b22ba96b8fae0b78875ee2a111fbb994345
Kardalfo <kardalfo@gmail.com>
2022-04-03 18:22:08 +0300

    [HMP] Start Window.

---
+++ Assets/Scripts/Windows/Start/StartWindow.cs
@@ -0,0 +1,18 @@
+using Gameplay;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Windows.Start
+{
+    public class StartWindow : BaseWindow
+    {
+        [SerializeField] private GameplayManager gameplayManager;
+        [SerializeField] private Button startButton;
+
+
+        private void Awake()
+        {
+            startButton.onClick.AddListener(gameplayManager.StartGame);
+        }
+    }
+}
